aa1893deb7d0d79f938790098dbb1e4b25e47dd0
Yusufjon Axmedov <163830632+Yusufjon1224@users.noreply.github.com>
2026-03-07 17:24:36 +0500

    implementation of generics

--- src/main/java/generics/Main.java
+++ src/main/java/generics/Main.java
@@ -1,15 +1,22 @@
 package generics;
 
-interface Player {}
+interface Player {
+
+    String name();
+}
 
 record BaseballPlayer(String name, String position) implements Player {}
 
 record FootballPlayer(String name, String position) implements Player {}
 
+record VolleyballPlayer(String name, String position) implements Player {}
+
 
 public class Main {
 
     public static void main(String[] args) {
+
+        var philly = new Affiliation("city", "Philadelphia, PA", "US");
 
         BaseballTeam phillies1 = new BaseballTeam("Philadelphia Phillies");
         BaseballTeam astros1 = new BaseballTeam("Houston Astros");
@@ -19,8 +26,8 @@ public class Main {
         SportsTeam astros2 = new SportsTeam("Houston Astros");
         scoreResult(phillies2, 3, astros2, 5);
 
-        Team<BaseballPlayer> phillies = new Team<>("Philadelphia Phillies");
-        Team<BaseballPlayer> astros = new Team<>("Houston Astros");
+        Team<BaseballPlayer, Affiliation> phillies = new Team<>("Philadelphia Phillies", philly);
+        Team<BaseballPlayer, Affiliation> astros = new Team<>("Houston Astros");
         scoreResult(phillies, 3, astros, 5);
 
         var harper = new BaseballPlayer("B Harper", "Right Fielder");
@@ -32,16 +39,24 @@ public class Main {
         phillies.listTeamMembers();
 
         SportsTeam afc1 = new SportsTeam("Adelaide Crows");
-        Team<FootballPlayer> afc = new Team<>("Adelaide Crows");
+        Team<FootballPlayer, String> afc = new Team<>("Adelaide Crows", "City of Adelaide, South Australia, in AU");
         var tex = new FootballPlayer(" Tex Walker", " Center half forward");
         afc.addTeamMember(tex);
         var rory = new FootballPlayer("Rory Laird", "Midfield");
         afc.addTeamMember(rory);
         afc.listTeamMembers();
 
-        Team<String> adelaide = new Team<>("Adelaide Storm");
-        adelaide.addTeamMember("N Roberts");
+        Team<VolleyballPlayer, Affiliation> adelaide = new Team<>("Adelaide Storm");
+        adelaide.addTeamMember(new VolleyballPlayer("N Roberts", "Setter"));
         adelaide.listTeamMembers();
+
+        var canberra  = new Team<VolleyballPlayer, Affiliation>("Canberra Heat");
+        canberra.addTeamMember(new VolleyballPlayer("B Black", "Opposite"));
+        canberra.listTeamMembers();
+        scoreResult(canberra, 0, adelaide, 1);
+
+//        Team<Integer> melbourneVB = new Team<>("Melbourne Vipers");
+
     }
 
     public static void scoreResult(BaseballTeam team1, int t1_score,
--- src/main/java/generics/Team.java
+++ src/main/java/generics/Team.java
@@ -3,17 +3,32 @@ package generics;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Team<T> {
+record Affiliation(String name, String type, String countryCode) {
+
+    @Override
+    public String toString() {
+        return name + " (" + type + " in " + countryCode + ")";
+    }
+}
+
+public class Team<T extends Player, S> {
 
     private String teamName;
     private List<T> teamMembers = new ArrayList<>();
     private int totalWins = 0;
     private int totalLosses = 0;
     private int totalTies = 0;
+    private S affiliation;
+
 
     // constructor for teamName
     public Team(String teamName) {
         this.teamName = teamName;
+    }
+
+    public Team(String teamName, S affiliation) {
+        this.teamName = teamName;
+        this.affiliation = affiliation;
     }
 
     public void addTeamMember(T t) {
@@ -26,8 +41,11 @@ public class Team<T> {
 
     public void listTeamMembers() {
 
-        System.out.println(teamName + " Roster:");
-        System.out.println(teamMembers);
+        System.out.print(teamName + " Roster:");
+        System.out.println(affiliation == null ? "" : " AFFILIATION: " + affiliation);
+        for (T t : teamMembers) {
+            System.out.println(t.name());
+        }
     }
 
     public int ranking() {
