1923a678305e442e4804ef1b6aa283a0fa0f330a
Yusufjon Axmedov <163830632+Yusufjon1224@users.noreply.github.com>
2026-03-09 19:17:44 +0500

    learning nested classes

--- src/main/java/nestedclasses/Main.java
+++ src/main/java/nestedclasses/Main.java
@@ -1,5 +1,6 @@
 package nestedclasses;
 
+import nestedclasses.burger.Store;
 import nestedclasses.domain.Employee;
 import nestedclasses.domain.EmployeeComparator;
 import nestedclasses.domain.StoreEmployee;
@@ -43,6 +44,47 @@ public class Main {
 
         for (StoreEmployee e : storeEmployees) {
             System.out.println(e);
+        }
+
+        System.out.println("With Pig Latin Names");
+        addPigLatinName(storeEmployees);
+    }
+
+    public static void addPigLatinName(List<? extends StoreEmployee> list) {
+
+        String lastName = "Piggy";
+
+        class DecoratedEmployee extends StoreEmployee implements Comparable<DecoratedEmployee> {
+            private String pigLatinName;
+            private Employee originalInstance;
+
+            public DecoratedEmployee(String pigLatinName, Employee originalInstance) {
+                this.pigLatinName = pigLatinName + " " + lastName;
+                this.originalInstance = originalInstance;
+            }
+
+            @Override
+            public String toString() {
+                return originalInstance.toString() + " " + pigLatinName;
+            }
+
+            @Override
+            public int compareTo(DecoratedEmployee o) {
+                return pigLatinName.compareTo(o.pigLatinName);
+            }
+        }
+
+        List<DecoratedEmployee> newList = new ArrayList<>(list.size());
+
+        for (var employee : list) {
+            String name = employee.getName();
+            String pigLatin = name.substring(1) + name.charAt(0) + "ay";
+            newList.add(new DecoratedEmployee(pigLatin, employee));
+        }
+        
+        newList.sort(null);
+        for (var dEmployee : newList) {
+            System.out.println(dEmployee.originalInstance.getName() + " " + dEmployee.pigLatinName);
         }
     }
 }
