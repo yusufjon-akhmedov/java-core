15d45e31b644c4a2be205449ea6a6e88c85fd3ab
Yusufjon Axmedov <163830632+Yusufjon1224@users.noreply.github.com>
2026-03-11 16:58:00 +0500

    learning method references in lambda

--- src/main/java/lambda/lambdaExpressions/MethodReferences.java
+++ src/main/java/lambda/lambdaExpressions/MethodReferences.java
@@ -3,8 +3,7 @@ package lambda.lambdaExpressions;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 class PlainOld {
 
@@ -34,6 +33,34 @@ public class MethodReferences {
         System.out.println("--> Getting array");
 
         PlainOld[] pojo1 = seedArray(PlainOld::new, 10);
+
+
+        // that one pure lambda
+        calculator((s1, s2) -> s1.concat(s2), "Hello", "World");
+
+        // that one method reference
+        calculator(String::concat, "Hello", "World");
+
+        BinaryOperator<String> b1 = String::concat;
+
+        BiFunction<String, String, String> b2 = String::concat;
+
+        UnaryOperator<String> u1 = String::toUpperCase;
+
+        System.out.println(b1.apply("Hello " , "World"));
+        System.out.println(b2.apply("Hello " ,  "World"));
+        System.out.println(u1.apply("Hello"));
+
+        String result = "Hello".transform(u1);
+        System.out.println("Result = " + result);
+
+        result = result.transform(String::toLowerCase);
+        System.out.println("Result = " + result);
+
+        Function<String, Boolean> f0 = String::isEmpty;
+        boolean resultBoolean = result.transform(f0);
+        System.out.println("Result = " + resultBoolean);
+
     }
 
     private static <T> void calculator(BinaryOperator<T> function, T value1, T value2) {
