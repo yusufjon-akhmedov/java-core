1a71de1cfdf187eeeefd3a8c25f39fa8c04fc67f
Yusufjon Axmedov <163830632+Yusufjon1224@users.noreply.github.com>
2026-03-11 23:39:01 +0500

    implementation of collections

---
+++ src/main/java/collections/Main.java
@@ -0,0 +1,26 @@
+package collections;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Collection<String> list = new HashSet<>();
+        String[] names = {"Anna", "Bob", "Carol", "David", "Edna"};
+        list.addAll(Arrays.asList(names));
+
+        System.out.println(list);
+
+        list.add("Fred");
+        list.addAll(Arrays.asList("George", "Gary", "Grace"));
+        System.out.println(list);
+        System.out.println("Gary is in the list? " + list.contains("Gary"));
+
+        list.removeIf(s -> s.charAt(0) == 'G');
+
+        System.out.println(list);
+        System.out.println("Gary is in the list? " + list.contains("Gary"));
+
+    }
+}
