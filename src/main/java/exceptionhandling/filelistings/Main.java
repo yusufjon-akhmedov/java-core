544cf020eaba95e4023811958f32eb6f523d2955
Yusufjon Axmedov <163830632+Yusufjon1224@users.noreply.github.com>
2026-03-15 23:32:59 +0500

    learning listing files in i/o operations

---
+++ src/main/java/exceptionhandling/filelistings/Main.java
@@ -0,0 +1,53 @@
+package exceptionhandling.filelistings;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Path path = Path.of("");
+        System.out.println("pwd = " + path.toAbsolutePath());
+
+        try (Stream<Path> paths = Files.list(path)) {
+            paths
+                    .map(Main::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("---------------------------------------------");
+
+        try (Stream<Path> paths = Files.walk(path, 2)) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .map(Main::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String listDir(Path path) {
+        try {
+            boolean isDir = Files.isDirectory(path);
+            FileTime dateField = Files.getLastModifiedTime(path);
+            LocalDateTime modDT = LocalDateTime.ofInstant(
+                    dateField.toInstant(), ZoneId.systemDefault());
+            return "%tD %tT %-5s %12s %s"
+                    .formatted(modDT, modDT, (isDir ? "<DIR>" : ""), (isDir ? "" : Files.size(path)), path);
+
+        } catch (Exception e) {
+            System.out.println("Whoops! Something went wrong with " + path);
+            return path.toString();
+        }
+    }
+}
