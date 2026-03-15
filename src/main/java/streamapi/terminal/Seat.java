d66b2e86bc9bd1594f84dd63b6c772351f4ec0ba
Yusufjon Axmedov <163830632+Yusufjon1224@users.noreply.github.com>
2026-03-15 05:37:24 +0500

    learning stream

---
+++ src/main/java/streamapi/terminal/Seat.java
@@ -0,0 +1,11 @@
+package streamapi.terminal;
+
+import java.util.Random;
+
+public record Seat(char rowMarker, int seatNumber, boolean isReserved) {
+
+    public Seat(char rowMarker, int seatNumber) {
+//        this(rowMarker, seatNumber, new Random().nextBoolean());
+        this(rowMarker, seatNumber, false);
+    }
+}
